2c86abb5c6c28f0e6e502b25ecce2149316a32b7
1337fury <elouardyabdelhalim@gmail.com>
2024-05-09 17:34:06 +0100

    Add create_post view for post creation using PostForm

--- website/main/views.py
+++ website/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .forms import PostForm
 # import login_required decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
@@ -19,3 +20,16 @@ def sign_up(request):
     else:
         form = RegistrationForm()
     return render(request, "registration/sign_up.html", {"form": form})
+
+@login_required(login_url="create_post")
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False) # commit=False means that the Post object is created but not saved to the database
+            post.author = request.user
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostForm()
+    return render(request, "main/create_post.html", {"form": form})
