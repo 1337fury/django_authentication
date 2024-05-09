f25fc738d2d6758c75164bc729a1a714cc522728
1337fury <elouardyabdelhalim@gmail.com>
2024-05-09 17:12:33 +0100

    Refactor RegistrationForm, add PostForm for post creation

--- website/main/forms.py
+++ website/main/forms.py
@@ -1,33 +1,28 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 
-class RegistrationForm(UserCreationForm):
+class RegistrationForm(UserCreationForm): # UserCreationForm is a form that is provided by Django that is used for creating new users
 	email = forms.EmailField(required=True)
 	
 	class Meta:
 		model = User
 		fields = ["username", "email", "password1", "password2"]
-		# labels = {
-		# 	"username": "Username",
-		# 	"email": "Email",
-		# 	"password1": "Password",
-		# 	"password2": "Confirm Password"
-		# }
-		# help_texts = {
-		# 	"username": None,
-		# 	"email": None,
-		# 	"password1": None,
-		# 	"password2": None
-		# }
-		# error_messages = {
-		# 	"username": {
-		# 		"unique": "This username is already taken."
-		# 	}
-		# }
 	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.fields["username"].widget.attrs.update({
+			"autofocus": True
+		})
+
+class PostForm(forms.ModelForm): # forms.ModelForm is a class that inherits from forms.Form and is used for creating forms that interact with models
+	class Meta:
+		model = Post
+		fields = ["title", "content"]
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields["title"].widget.attrs.update({
 			"autofocus": True
 		})
