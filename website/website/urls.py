5b9832063265c554781450eddb7f30d885f8874f
1337fury <elouardyabdelhalim@gmail.com>
2024-05-09 11:54:29 +0100

    Include Django authentication URLs in project urlpatterns

--- website/website/urls.py
+++ website/website/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')), 
 ]
