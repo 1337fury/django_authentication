d7438e29c1a274a757a0cef0f3b1cfce9652811a
1337fury <elouardyabdelhalim@gmail.com>
2024-05-09 15:18:25 +0100

    Define Post model with fields for title, content, author, and date_created

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	author = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete=models.CASCADE means that if the user is deleted, all of their posts will be deleted as well
+	date_created = models.DateTimeField(auto_now_add=True) # auto_now_add=True means that the date_created field will be automatically set to the current date and time when a new Post object is created
+
+	def __str__(self):
+		return self.title + " | " + str(self.author)
